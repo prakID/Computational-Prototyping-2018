09b163d11be3f8fdeb3f392833b894ed1f353269
Prak <mail@prakash.design>
2018-03-15 20:11:23 +1100

    Random edits

--- Sketching_draw_rectangles/Sketching_draw_rectangles.js
+++ Sketching_draw_rectangles/Sketching_draw_rectangles.js
@@ -2,19 +2,20 @@ var vi;
 
 function setup() {
 createCanvas(1920,720);
+rectMode(CORNERS);
 }
 
 function draw() {
-  
+
 }
 
 function mousePressed() {
- vi = createVector(mouseX,mouseY); 
+ vi = createVector(mouseX,mouseY);
  print(vi.x,vi.y);
 }
 
 function mouseReleased() {
   stroke(0);
   noFill();
- rect(vi.x, vi.y, +(mouseX-vi.x), +(mouseY-vi.y)); 
+ rect(vi.x, vi.y, mouseX, mouseY);
 }
